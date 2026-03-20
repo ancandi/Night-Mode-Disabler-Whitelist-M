3605da39bf1c7b42c1958d6d0996f1ad009be613
and1 <131588191+ancandi@users.noreply.github.com>
2026-03-20 23:32:20 +0800

    Create night-mode-disabler-safari.user.js

---
+++ night-mode-disabler-safari.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name        Night-Mode-Disabler-S
+// @version     2.1.5
+// @run-at      document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    if (!/youtube\.com|google\.com/.test(location.hostname)) return;
+
+    const d = document.documentElement,
+          s = document.createElement('style');
+
+    s.textContent = ':root,html,body{background:#fff!important;color-scheme:light!important;filter:none!important;-webkit-filter:none!important}*{filter:none!important;-webkit-filter:none!important}';
+
+    const f = () => {
+        if (!d.contains(s)) d.append(s);
+        d.setAttribute('data-apple-color-scheme', 'light');
+    };
+
+    f();
+    d.addEventListener('DOMContentLoaded', f);
+})();
